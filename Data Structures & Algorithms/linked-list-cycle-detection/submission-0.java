98903a65a29133a7c15c379939963520e601f7fb
ANSHAL MISHRA <anshalmishra8@gmail.com>
2026-05-30 22:54:36 +0530

    Add: linked-list-cycle-detection - submission-0

---
+++ Data Structures & Algorithms/linked-list-cycle-detection/submission-0.java
@@ -0,0 +1,17 @@
+class Solution {
+    public boolean hasCycle(ListNode head) {
+        ListNode pt1 = head;
+        ListNode pt2 = head;
+
+        while (pt2 != null && pt2.next != null) {
+            pt1 = pt1.next;
+            pt2 = pt2.next.next;
+
+            if (pt1 == pt2) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+}
