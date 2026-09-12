e050498b5db0bbbaa85ccef0fb2e0d41787bed62
ANSHAL MISHRA <anshalmishra8@gmail.com>
2026-09-13 04:29:40 +0530

    Add: n-queens - submission-0

---
+++ Data Structures & Algorithms/n-queens/submission-0.java
@@ -0,0 +1,44 @@
+class Solution {
+    public List<List<String>> solveNQueens(int n) {
+        List<List<String>> res = new ArrayList<>();
+        List<String> subans = new ArrayList<>();
+        backt(res, subans, n, 0);
+        return res;
+    }
+
+    public void backt(List<List<String>> res, List<String> subans,int n, int row) {
+        if (subans.size() == n) {
+            res.add(new ArrayList<>(subans));
+            return;
+        }
+
+        String proxy = ".".repeat(n);
+
+        for (int i = 0; i < n; i++) {
+            if (isSafe(subans, row, i, n)) {
+                String str = proxy.substring(0, i) + "Q" + proxy.substring(i + 1);
+                subans.add(str);
+                backt(res, subans, n, row + 1);
+                subans.remove(subans.size() - 1);
+            }
+        }
+    }
+
+    public boolean isSafe(List<String> subans, int row, int col, int n) {
+
+        for (int prevRow = 0; prevRow < subans.size(); prevRow++) {
+            String previous = subans.get(prevRow);
+
+            if (previous.charAt(col) == 'Q') {
+                return false;
+            }
+
+            int prevCol = previous.indexOf('Q');
+            if (Math.abs(row - prevRow) == Math.abs(col - prevCol)) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
